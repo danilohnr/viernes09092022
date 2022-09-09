9d4ffc1df9e62baa43c736dd9eef1ae8a830c78f
Danilo Noguera <danilo.noguera@gmail.com>
2022-09-09 16:42:40 -0600

    Creación de la clase EstructuraFor

--- src/estructurapostprueba/EstructuraDoWhile.java
+++ src/estructurapostprueba/EstructuraDoWhile.java
@@ -3,8 +3,26 @@ package estructurapostprueba;
 public class EstructuraDoWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*Estructuras repetitivas
+		 * -Estructuras preprueba
+		 * ---Estructura while
+		 * -Estructura postprueba
+		 * ---Estructura do ... while */
+		int num = 5;
+		System.out.println("Ciclo While");
+		while (num < 10) { //prueba
+			num++;
+			System.out.println("num = " + num);
+		}
+		//Con el do while
+		System.out.println("Ciclo Do While");
+		do {
+			num++;
+			System.out.println("num = " + num);
+		} while (num < 10);
+		
+		
+		System.out.println("Fin del programa.");
 	}
 
 }
---
+++ src/estructurapostprueba/EstructuraFor.java
@@ -0,0 +1,10 @@
+package estructurapostprueba;
+
+public class EstructuraFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
